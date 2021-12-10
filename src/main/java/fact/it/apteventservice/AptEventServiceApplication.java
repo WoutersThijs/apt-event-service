d65e7ee08147830290b73e71775cd3c705bfea07
Thijs <r0793167@student.thomasmore.be>
2021-12-10 15:36:17 +0100

    Init project

---
+++ src/main/java/fact/it/apteventservice/AptEventServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.apteventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptEventServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptEventServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/apteventservice/AptEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.apteventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptEventServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
